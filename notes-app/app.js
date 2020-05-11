682c8f0397a47a32249b91f55f18ca83671166ac
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-11 19:30:26 -0400

    Importing npm Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,11 @@
+
+const validator = require('validator');
+
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 console.log(msg);
+
+
+console.log(validator.isEmail("mattlarrubia@gmail.com"))
+console.log(validator.isURL('matthewlarrubia.dev'));

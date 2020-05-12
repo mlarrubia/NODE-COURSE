dffafd7e3a345d304eb8e698b18d934e29a911f5
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-11 20:01:55 -0400

    Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,9 @@
-
-const validator = require('validator');
-
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
-console.log(msg);
+console.log(chalk.dim.bgRed(msg));
+console.log(chalk.dim.red.bgBlue.bold.underline(msg));
+console.log(chalk.bgMagenta(msg));
 
 
-console.log(validator.isEmail("mattlarrubia@gmail.com"))
-console.log(validator.isURL('matthewlarrubia.dev'));

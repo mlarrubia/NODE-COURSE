79c0df2dd2da27898bcc083795d44837a7029fed
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-11 20:12:56 -0400

    Global npm Nodules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
-console.log(chalk.dim.bgRed(msg));
+console.log(chalk.bold.bgBlue(msg));
 console.log(chalk.dim.red.bgBlue.bold.underline(msg));
 console.log(chalk.bgMagenta(msg));
 

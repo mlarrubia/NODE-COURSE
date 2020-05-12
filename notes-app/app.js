d81e7b6b007fbadff72b0abed330a0afb828a414
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-11 23:11:46 -0400

    Argument Parsing with Yargs Part I

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,43 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
-console.log(process.argv[0])
-if (command === 'add') {
-    console.log("Adding note!");
-} else if (command === 'remove') {
-    console.log("Removing note!")
-}
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    description: "add a new note",
+    handler: function () {
+        console.log("Adding a new note!")
+    }
+})
+
+// Create remove command 
+yargs.command({
+    command: 'remove',
+    description: "remove a note",
+    handler: function () {
+        console.log("Removing the note")
+    }
+})
+
+// Create List command
+yargs.command({
+    command: 'list',
+    description: "List your note",
+    handler: function () {
+        console.log("Listing out all the note")
+    }
+})
+
+// Create Read command
+yargs.command({
+    command: 'read',
+    description: "Read a note",
+    handler: function () {
+        console.log("Reading the note")
+    }
+})
+// add, remove, read, list
+console.log(yargs.argv);

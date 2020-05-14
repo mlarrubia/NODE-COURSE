02ce5f5946ecf518f4804485bda20e06aef7e6ff
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-14 14:46:35 -0400

    Customizing HTTP Request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,15 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=a03e3470eaa547e651a47fd3880546d6&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=a03e3470eaa547e651a47fd3880546d6&query=25.761681,-80.191788&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.location.name)
+request({ url: url, json: true }, (error, response) => {
+
+    const actualTemp = response.body.current.temperature;
+    const feelsLikeTemp = response.body.current.feelslike;
+
+    console.log(`${response.body.current.weather_descriptions[0]} It is currently ${actualTemp} degrees out. It feels like ${feelsLikeTemp} degrees out`)
 })
+
+function celciusToFahrenheit(cel) {
+    return (cel * 9 / 5) + 32;
+}

cb9f2fa434eb2ecaa877c088e2d4410398776859
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-16 00:11:02 -0400

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,32 @@
-// Core modules
 const path = require('path');
-// NPM Modules
 const express = require('express');
 
+const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
-const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "Matthew Larrubia"
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: "Matthew Larrubia"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: "This is the help page"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({

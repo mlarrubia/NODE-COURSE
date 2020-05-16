21be35cad2a9f2b860a568e79d0d307395736f81
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-15 21:46:27 -0400

    Serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,14 @@
+// Core modules
+const path = require('path');
+// NPM Modules
 const express = require('express');
+
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Weather</h1>');
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Andrew',
-        age: 27
-    });
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
-})
 
 app.get('/weather', (req, res) => {
     res.send({

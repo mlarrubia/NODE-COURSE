8e00f7cfafbb570001b97ffdb885b76dff40d459
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-16 17:20:34 -0400

    Advanced Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -24,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: "This is the help page"
+        message: "This is the help page",
+        title: "Help",
+        name: "Matthe Larrubia"
     })
 })
 

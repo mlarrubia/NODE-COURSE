6666d8e71cf565a728830c31bab3ea9b6a8beb9c
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-15 21:18:16 -0400

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('<h1>Weather</h1>');
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help Page');
+    res.send({
+        name: 'Andrew',
+        age: 27
+    });
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.send('<h1>About Page</h1>');
 })
 
 app.get('/weather', (req, res) => {
-    res.send('View Weather!');
+    res.send({
+        forecast: "Raining",
+        location: "Miami"
+    });
 })
 // app.com
 

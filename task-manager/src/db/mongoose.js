fe48fa249d572c5c9cb77b1e6cb1651b9bf22ada
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-24 11:12:15 -0400

    Resource Reading Endpoints: Pt. II

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,24 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task1 = new Task({
-    description: "Eat dinner"
-})
-
-task1.save().then((result) => {
-    console.log(result)
-}).catch((error) => {
-    console.log(error)
-})

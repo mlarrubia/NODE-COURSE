b95b5e6c8f344a04060d543e8e034899f33bfb39
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-25 11:42:35 -0400

    Resource Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -47,6 +47,46 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Updating Existing Resource
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates!" })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -85,30 +125,6 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
-// Updating Existing Resource
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => {
-        return allowedUpdates.includes(update)
-    })
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: "Invalid updates!" })
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
 
 
 app.patch('/tasks/:id', async (req, res) => {
@@ -131,6 +147,21 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(500).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+        return res.status(404).send({ error: "Task not found" })
+    }
+
+    res.send(task)
+
+    try {
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
     }
 })
 

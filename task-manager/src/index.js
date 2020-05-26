8ec5ffc6a978041ea9acabd430b89f1e77700cd9
Matthew Larrubia <mlarrubia@columbushs.com>
2020-05-25 23:15:20 -0400

    Securely Storing Passwords Pt. II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,11 +21,7 @@ const myFunction = async () => {
     const password = 'Red12345!'
     const hashedPW = await bcrypt.hash(password, 8)
 
-    console.log(password)
-    console.log(hashedPW)
-
     const isMatch = await bcrypt.compare('Red12345!', hashedPW)
-    console.log(isMatch)
 }
 
 myFunction()

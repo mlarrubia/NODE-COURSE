47eb945a5e19e72c27b43bc290b1b6ab7d99ec20
Matthew Larrubia <mlarrubia@columbushs.com>
2020-06-02 22:19:24 -0400

    Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET request are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Server is under maintanince')
+})
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
